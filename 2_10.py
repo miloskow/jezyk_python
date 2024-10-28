ad3bddbc7327cc77c834ddf8bbfbe2ee0fc9d671
miloskow <121123930+miloskow@users.noreply.github.com>
2024-10-28 18:12:23 +0100

    Delete 2_10.py

--- 2_10.py
+++
@@ -1,9 +0,0 @@
-line = """Przyklad wielowierszowego napisu
-        z kilkoma napisami
-        zrobiony w celu policzenia slow."""
-
-wyrazy = line.split()
-
-liczba_wyrazow = len(wyrazy)
-
-print("Liczba wyrazow: ", liczba_wyrazow)
