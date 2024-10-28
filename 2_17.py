a921cc7143215f1298cc1e9778ade7aa6337cbb2
miloskow <121123930+miloskow@users.noreply.github.com>
2024-10-28 18:14:26 +0100

    Delete 2_17.py

--- 2_17.py
+++
@@ -1,9 +0,0 @@
-line = "tekst do posortowania alfabetycznie i pod wzgledem dlugosci "
-
-wyrazy = line.split()
-
-posortowane = sorted(wyrazy)
-posortowane_po_dlugosci = sorted(wyrazy, key=len)
-
-print(posortowane)
-print(posortowane_po_dlugosci)
