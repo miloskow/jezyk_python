2dcfd0c28d02c6c0dd61e1d95bbdb098d8df1b03
miloskow <121123930+miloskow@users.noreply.github.com>
2025-01-27 12:12:56 +0100

    Zmiana na krotsze wiersze kodu

--- projekt/button.py
+++ projekt/button.py
@@ -13,16 +13,18 @@ class Button:
     def draw(self):
         """Rysuje przycisk, zmienia kolor po najechaniu myszką."""
         mouse_x, mouse_y = pygame.mouse.get_pos()
-        button_color = self.hover_color if self.rect.collidepoint(mouse_x, mouse_y) else self.color
+        button_color = (self.hover_color if self.rect.collidepoint(mouse_x, mouse_y) 
+                        else self.color)
         
         pygame.draw.rect(self.screen, button_color, self.rect)
-        pygame.draw.rect(self.screen, (0, 0, 0), self.rect, 2)  # Czarna ramka dla efektu
+        pygame.draw.rect(self.screen, (0, 0, 0), self.rect, 2) 
 
         text_surface = self.font.render(self.text, True, self.text_color)
-        text_x = self.rect.x + (self.rect.width - text_surface.get_width()) // 2
-        text_y = self.rect.y + (self.rect.height - text_surface.get_height()) // 2
+        text_x = (self.rect.x + (self.rect.width - text_surface.get_width()) // 2)
+        text_y = (self.rect.y + (self.rect.height - text_surface.get_height()) // 2)
         self.screen.blit(text_surface, (text_x, text_y))
 
     def is_clicked(self, event):
         """Sprawdza, czy przycisk został kliknięty."""
-        return event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos)
+        return (event.type == pygame.MOUSEBUTTONDOWN and 
+                self.rect.collidepoint(event.pos))
